65de4ebe4402d5e0538ffdd5001bedc22759e765
ViktorSvertoka <victor.svertoka@gmail.com>
2024-12-24 22:00:43 +0200

    Feat(PY) Change auth

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException, status, BackgroundTasks, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.security import OAuth2PasswordRequestForm
-from src.schemas import UserCreate, Token, User, RequestEmail
-from src.services.email import send_email
-from src.services.auth import create_access_token, Hash, get_email_from_token
+from src.schemas import UserCreate, Token, User, RequestEmail, ResetPassword
+from src.services.email import send_confirm_email, send_reset_password_email
+from src.services.auth import (
+    create_access_token,
+    Hash,
+    get_email_from_token,
+    get_password_from_token,
+)
 from src.services.users import UserService
 from src.database.db import get_db
 
@@ -35,7 +40,7 @@ async def register_user(
     user_data.password = Hash().get_password_hash(user_data.password)
     new_user = await user_service.create_user(user_data)
     background_tasks.add_task(
-        send_email, new_user.email, new_user.username, request.base_url
+        send_confirm_email, new_user.email, new_user.username, request.base_url
     )
 
     return new_user
@@ -71,7 +76,7 @@ async def confirmed_email(token: str, db: AsyncSession = Depends(get_db)):
     user = await user_service.get_user_by_email(email)
     if user is None:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Verification error"
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Помилка підтвердження"
         )
     if user.confirmed:
         return {"message": "Ваша електронна пошта вже підтверджена"}
@@ -93,6 +98,68 @@ async def request_email(
         return {"message": "Ваша електронна пошта вже підтверджена"}
     if user:
         background_tasks.add_task(
-            send_email, user.email, user.username, request.base_url
+            send_confirm_email, user.email, user.username, request.base_url
         )
     return {"message": "Перевірте свою електронну пошту для підтвердження"}
+
+
+@router.post("/reset_password")
+async def reset_password_request(
+    body: ResetPassword,
+    background_tasks: BackgroundTasks,
+    request: Request,
+    db: AsyncSession = Depends(get_db),
+):
+
+    user_service = UserService(db)
+    user = await user_service.get_user_by_email(body.email)
+
+    if not user:
+        return {"message": "Перевірте свою електронну пошту для підтвердження"}
+
+    if not user.confirmed:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Ваша електронна пошта не підтверджена",
+        )
+
+    hashed_password = Hash().get_password_hash(body.password)
+
+    reset_token = await create_access_token(
+        data={"sub": user.email, "password": hashed_password}
+    )
+
+    background_tasks.add_task(
+        send_reset_password_email,
+        to_email=body.email,
+        username=user.username,
+        host=str(request.base_url),
+        reset_token=reset_token,
+    )
+
+    return {"message": "Перевірте свою електронну пошту для підтвердження"}
+
+
+@router.get("/confirm_reset_password/{token}")
+async def confirm_reset_password(token: str, db: AsyncSession = Depends(get_db)):
+    email = await get_email_from_token(token)
+    hashed_password = await get_password_from_token(token)
+
+    if not email or not hashed_password:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Недійсний або прострочений токен",
+        )
+
+    user_service = UserService(db)
+    user = await user_service.get_user_by_email(email)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Користувача з такою електронною адресою не знайдено",
+        )
+
+    await user_service.reset_password(user.id, hashed_password)
+
+    return {"message": "Пароль успішно змінено"}
