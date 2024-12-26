387afa962378a189fd5d6c2d46a3fbd40670eb0a
ViktorSvertoka <victor.svertoka@gmail.com>
2024-12-26 22:34:25 +0200

    Feat(PY) Change auth

--- src/services/auth.py
+++ src/services/auth.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from typing import Optional
+from aiocache import cached
 from fastapi import Depends, HTTPException, status
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
@@ -7,6 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from jose import JWTError, jwt
 
 from src.database.db import get_db
+from src.database.models import User, UserRole
 from src.conf.config import settings
 from src.services.users import UserService
 
@@ -14,17 +16,17 @@ from src.services.users import UserService
 class Hash:
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-    def verify_password(self, plain_password, hashed_password):
+    def verify_password(self, plain_password, hashed_password) -> bool:
         return self.pwd_context.verify(plain_password, hashed_password)
 
-    def get_password_hash(self, password: str):
+    def get_password_hash(self, password: str) -> str:
         return self.pwd_context.hash(password)
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 
 
-async def create_access_token(data: dict, expires_delta: Optional[int] = None):
+async def create_access_token(data: dict, expires_delta: Optional[int] = None) -> str:
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.now(timezone.utc) + timedelta(seconds=expires_delta)
@@ -39,9 +41,24 @@ async def create_access_token(data: dict, expires_delta: Optional[int] = None):
     return encoded_jwt
 
 
+def cache_key_builder(func, args, kwargs) -> str:
+
+    return f"username: {args[0]}"
+
+
+@cached(ttl=300, key_builder=cache_key_builder)
+async def get_user_from_db(username: str, db: AsyncSession) -> User:
+
+    print("NOT CACHED USER")
+    user_service = UserService(db)
+    user = await user_service.get_user_by_username(username)
+
+    return user
+
+
 async def get_current_user(
     token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)
-):
+) -> User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -64,7 +81,14 @@ async def get_current_user(
     return user
 
 
-def create_email_token(data: dict):
+def get_current_admin_user(current_user: User = Depends(get_current_user)) -> User:
+
+    if current_user.role != UserRole.ADMIN:
+        raise HTTPException(status_code=403, detail="Permission denied")
+    return current_user
+
+
+def create_email_token(data: dict) -> str:
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(days=7)
     to_encode.update({"iat": datetime.now(timezone.utc), "exp": expire})
@@ -72,7 +96,7 @@ def create_email_token(data: dict):
     return token
 
 
-async def get_email_from_token(token: str):
+async def get_email_from_token(token: str) -> str:
     try:
         payload = jwt.decode(
             token, settings.JWT_SECRET, algorithms=[settings.JWT_ALGORITHM]
@@ -83,4 +107,19 @@ async def get_email_from_token(token: str):
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             detail="Неправильний токен для перевірки електронної пошти",
+        )
+
+
+async def get_password_from_token(token: str) -> str:
+
+    try:
+        payload = jwt.decode(
+            token, settings.JWT_SECRET, algorithms=[settings.JWT_ALGORITHM]
+        )
+        password = payload["password"]
+        return password
+    except JWTError as e:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Wrong token",
         )
